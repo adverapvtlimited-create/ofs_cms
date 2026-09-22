cbaab843024debf40e0a10481a60e037c83c022e
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2026-09-22 22:13:09 +0530

    feat: add additional CORS origin for project-specific access

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,6 +9,7 @@ module.exports = [
       origin: [
         'https://ofsgroupindia.in',
         'https://www.ofsgroupindia.in',
+        'https://ofs-git-feat-crm-adverapvtlimited-creates-projects.vercel.app'
       ],
       credentials: true,
     },
