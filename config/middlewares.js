a8034b19982cf0eca38950f922c06f1245ed2196
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2026-09-22 22:03:39 +0530

    feat: restructure CORS middleware configuration for improved clarity

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,18 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://ofsgroupindia.in',
+        'https://www.ofsgroupindia.in',
+      ],
+      credentials: true,
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
